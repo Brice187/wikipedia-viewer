18415b7bc0347cea98459ca4f7e4063af3ae38de
Brice187 <lars@kerick.net>
2016-06-24 23:14:43 +0200

    Functionality is nearly finished. As always the design has to be improved!!!

--- scripts/wikiviewer.js
+++ scripts/wikiviewer.js
@@ -1,5 +1,34 @@
-function openRandomWiki() {
-	'use strict';
-	var win = window.open("https://en.wikipedia.org/wiki/Special:Random", '_blank');
-	win.focus();
-}
+$(document).ready(function () {
+	$("#randomWiki").click(function (event) {
+		var url = "https://en.wikipedia.org/wiki/Special:Random";
+		window.open(url, '_blank');
+
+	});
+
+	$("#searchButton").click(function (event) {
+		var searchstring = $("#searchText");
+		$.ajax({
+				url: '//en.wikipedia.org/w/api.php',
+				data: {
+					action: 'query',
+					list: 'search',
+					srsearch: searchstring.val(),
+					format: 'json'
+				},
+				dataType: "jsonp"
+			})
+			.done(function (json) {
+				$("#content").prepend("<div class=\"new-entry-box\"><h3>" + json.query.search[0].title + "</h3>" + json.query.search[0].snippet + "</div>");
+				$("#content").animate({
+					scrollTop: $("#content").scrollHeight
+				}, 500);
+
+			})
+			.fail(function (xhr, status, errorThrown) {
+				alert("Sorry, there was a problem! Error: " + errorThrown);
+				console.log("Error: " + errorThrown);
+				console.log("Status: " + status);
+				console.dir(xhr);
+			});
+	});
+});
